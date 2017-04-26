4d017af4b1e8e704cc8e44adb65397d76ece9097
Vladimir Potter <v@mrpotter.ru>
2017-04-26 19:24:53 +0700

    Add AdminUser model. Use it as AUTH_USER_MODEL

--- user_administration/settings.py
+++ user_administration/settings.py
@@ -81,9 +81,7 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = "users.AdminUser"
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- users/models.py
+++ users/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 from localflavor.generic.models import IBANField
 
 
+class AdminUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+
+
 class User(models.Model):
+    creator = models.ForeignKey(AdminUser)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     iban = IBANField()
