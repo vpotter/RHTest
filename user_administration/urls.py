b7926d72682c380f1a6fe8592f9fccf5a0f7bfe9
Vladimir Potter <v@mrpotter.ru>
2017-04-26 19:26:51 +0700

    Add users API endpoint

--- user_administration/urls.py
+++ user_administration/urls.py
@@ -1,22 +1,11 @@
-"""user_administration URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 
-from users import urls as _  # noqa
+from users.views import UserViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r'users', UserViewSet, base_name='users')
 
 urlpatterns = [
-    url(r'^users', include('users.urls')),
+    url(r'^api', include(router.urls, namespace='api')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import (
+    ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin,
+    DestroyModelMixin)
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(ListModelMixin, RetrieveModelMixin, CreateModelMixin,
+                  UpdateModelMixin, DestroyModelMixin, GenericViewSet):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(creator=self.request.user)
