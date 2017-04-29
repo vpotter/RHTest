06fd2eeaf33ea75b1e45b9dcd800fbd21e6fe55e
Vladimir Potter <v@mrpotter.ru>
2017-04-29 22:05:30 +0700

    Add IBAN validator to user details form

---
+++ frontend/app/user-details/user-details.iban.validator.js
@@ -0,0 +1,22 @@
+angular
+    .module('userDetails')
+    .directive('iban', function (){
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attr, ngModel) {
+                var iban = attr.iban;
+
+                var validator = function(value) {
+                    var valid = IBAN.isValid(value);
+                    ngModel.$setValidity('iban', valid);
+                    return valid ? value : undefined;
+                };
+
+                //For DOM -> model validation
+                ngModel.$parsers.unshift(validator);
+
+                //For model -> DOM validation
+                ngModel.$formatters.unshift(validator);
+            }
+        };
+    });
