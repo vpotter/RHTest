f11759836d4a8ce98078ee513106dca82210c1a7
Vladimir Potter <v@mrpotter.ru>
2017-04-30 15:18:39 +0700

    Add Sign Out button. display current user in toolbar

--- frontend/app/app.module.js
+++ frontend/app/app.module.js
@@ -1,4 +1,17 @@
 'use strict';
 
 angular
-    .module('userAdminApp', ['ngMaterial', 'ngMessages', 'ngRoute', 'ngResource', 'userList', 'userDetails', 'exceptionHandler']);
+    .module('userAdminApp', ['ngMaterial', 'ngMessages', 'ngRoute', 'ngResource', 'userList', 'userDetails', 'exceptionHandler'])
+    .controller('userAdminController', ['$rootScope', 'authService', function($rootScope, authService){
+        var self = this;
+
+        authService.init();
+
+        self.signOut = function() {
+            console.log('sign out');
+            if ($rootScope.current_user) {
+                authService.signOut();
+            }
+            window.location = '/';
+        };
+    }]);
